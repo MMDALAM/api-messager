7f096e1c968f0daa947bc7fa57e00bf95f5dac1b
mohammad alamzadeh <mmmohammadalam@gmail.com>
2024-10-27 12:11:40 +0330

    auth

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,15 +1,14 @@
 const Joi = require('joi');
 
 const authSchema = Joi.object({
-  username: Joi.string().min(5).max(30).required().messages({
-    'string.min': 'نام کاربری نمیتواند کمتر از 5 کاراکتر باشد',
-    'string.max': 'نام کاربری نمیتواند بیشتر از 30 کاراکتر باشد',
-    'string.empty': 'رمزعبور نمیتواند خالی باشد ',
+  username: Joi.string().max(50).required().messages({
+    'string.max': 'Username cannot be more than 50 characters',
+    'string.empty': 'Username cannot be empty',
   }),
   password: Joi.string().min(5).max(20).required().messages({
-    'string.min': 'رمز عبور نمیتواند کمتر از 5 کاراکتر باشد',
-    'string.max': 'رمز عبور نمیتواند بیشتر از 20 کاراکتر باشد',
-    'string.empty': 'رمزعبور نمیتواند خالی باشد ',
+    'string.min': 'Password cannot be less than 5 characters',
+    'string.max': 'Password cannot be more than 20 characters',
+    'string.empty': 'Password cannot be empty',
   }),
 });
 
