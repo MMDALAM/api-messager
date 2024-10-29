dc461ff0d14aace66ccb5ebe1cd7a053805ff2d6
mohammad alamzadeh <mmmohammadalam@gmail.com>
2024-10-29 11:31:50 +0330

    auth

--- src/middlewares/auth.widdleware.js
+++ src/middlewares/auth.widdleware.js
@@ -3,19 +3,22 @@ const createError = require('http-errors');
 const userModel = require('../models/user.model');
 
 function tokens(headers) {
+  if (!headers['authorization']) return createError(401, 'Access denied. No token provided.');
   const token = headers['authorization'].split(' ')[1] || [];
   if (!token) return createError(401, 'Access denied. No token provided.');
   return token;
 }
 exports.verifyUser = async (req, res, next) => {
   try {
+    if (!req.headers['authorization']) return res.status(403).json({ message: 'Access denied. No token provided.' });
     const token = tokens(req?.headers);
+
     JWT.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET_USER, async (err, paylod) => {
-      if (err) return res.status(403).json({ error: 'Invalid token.' });
+      if (err) return res.status(403).json({ message: 'Invalid token.' });
       const user = await userModel.findById(paylod.id, { __v: 0, updatedAt: 0 });
-      if (!user) return res.status(403).json({ error: 'Invalid token.' });
+      if (!user) return res.status(403).json({ message: 'Invalid token.' });
       if (user.token === token) return res.status(200).json({ message: 'Token is valid', user: user });
-      return res.status(403).json({ error: 'Token is not match' });
+      return res.status(403).json({ message: 'Token is not match' });
     });
   } catch (err) {
     next(err);
@@ -23,13 +26,15 @@ exports.verifyUser = async (req, res, next) => {
 };
 exports.verify = async (req, res, next) => {
   try {
+    if (!req.headers['authorization']) return res.status(403).json({ message: 'Access denied. No token provided.' });
     const token = tokens(req?.headers);
+
     JWT.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET_USER, async (err, paylod) => {
-      if (err) return res.status(403).json({ error: 'Invalid token.' });
+      if (err) return res.status(403).json({ message: 'Invalid token.' });
       const user = await userModel.findById(paylod.id, { __v: 0, updatedAt: 0 });
-      if (!user) return res.status(403).json({ error: 'Not fund user.' });
+      if (!user) return res.status(403).json({ message: 'Not fund user.' });
       if (user.token === token) next();
-      else return res.status(403).json({ error: 'Token is not match' });
+      else return res.status(403).json({ message: 'Token is not match' });
     });
   } catch (err) {
     next(err);
