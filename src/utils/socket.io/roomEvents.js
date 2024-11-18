f7af714fe6633d3e2f6636987d8fe3d8a614d522
mohammad alamzadeh <mmmohammadalam@gmail.com>
2024-11-18 16:21:26 +0330

    fix

--- src/utils/socket.io/roomEvents.js
+++ src/utils/socket.io/roomEvents.js
@@ -1,30 +1,30 @@
-const Room = require('../../models/room.model');
-const { handleSocketError, roomsLists } = require('./socketHandlers');
-const userModel = require('../../models/user.model');
-const userSocketMap = {};
+// const Room = require('../../models/room.model');
+// const { handleSocketError, roomsLists } = require('./socketHandlers');
+// const userModel = require('../../models/user.model');
+// const userSocketMap = {};
 
-const roomEventHandlesdsdrs = (socket, namespace, io, userId) => {
-  socket.on('createRoom', async (data) => {
-    try {
-      const name = data?.data?.name;
-      const members = data?.data?.members;
-      const admin = data?.data?.admin;
-      const newRoom = new Room({ name, members, admin });
-      await newRoom.save();
-      members.forEach(async (e) => {
-        await userModel.findByIdAndUpdate(e, {
-          $push: {
-            rooms: newRoom._id,
-          },
-        });
-      });
-      namespace.emit('rooms', newRoom);
-      console.log('send new room');
-      await roomsLists(io, userId);
-    } catch (error) {
-      console.log(error);
-      handleSocketError(socket, 'Failed to create room');
-    }
-  });
-};
-module.exports = roomEventHandlers;
+// const roomEventHandlesdsdrs = (socket, namespace, io, userId) => {
+//   socket.on('createRoom', async (data) => {
+//     try {
+//       const name = data?.data?.name;
+//       const members = data?.data?.members;
+//       const admin = data?.data?.admin;
+//       const newRoom = new Room({ name, members, admin });
+//       await newRoom.save();
+//       members.forEach(async (e) => {
+//         await userModel.findByIdAndUpdate(e, {
+//           $push: {
+//             rooms: newRoom._id,
+//           },
+//         });
+//       });
+//       namespace.emit('rooms', newRoom);
+//       console.log('send new room');
+//       await roomsLists(io, userId);
+//     } catch (error) {
+//       console.log(error);
+//       handleSocketError(socket, 'Failed to create room');
+//     }
+//   });
+// };
+// module.exports = roomEventHandlers;
