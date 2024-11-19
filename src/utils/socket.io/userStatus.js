0f1842253d3f892153187b0492722f5cded399ea
mohammad alamzadeh <mmmohammadalam@gmail.com>
2024-11-19 11:45:28 +0330

    fix

--- src/utils/socket.io/userStatus.js
+++ src/utils/socket.io/userStatus.js
@@ -13,10 +13,8 @@ const setUserStatus = async (userId, status, socket, io) => {
   try {
     await userModel.findByIdAndUpdate(userId, { status });
     const onlineUsers = await userModel.find({ status: 'online' }, { username: 1, status: 1 });
-    const allUsers = await userModel.find({}, { username: 1, status: 1 });
 
     io.emit('onlineUsers', onlineUsers);
-    io.emit('allUsers', allUsers);
   } catch (error) {
     handleSocketError(socket, 'Failed to update user status');
   }
