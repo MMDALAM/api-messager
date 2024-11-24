4f8544353cc7a9a57279ded35a9d42a653cf04f2
mohammad alamzadeh <mmmohammadalam@gmail.com>
2024-11-24 10:10:06 +0330

    message

--- src/controllers/api/message.controller.js
+++ src/controllers/api/message.controller.js
@@ -1,25 +1,27 @@
 const controller = require('../contoller');
 const { isValidMongoId } = require('../../utils/function');
 const Message = require('../../models/message.model');
+const Room = require('../../models/room.model');
 
 class messageController extends controller {
-  async findManyRooms(req, res, next) {
+  async findMany(req, res, next) {
     try {
       const { id } = req.params;
       if (!isValidMongoId(id)) return res.status(200).json({ message: 'Not Valid MongoDB' });
+
+      const room = await Room.findById(id);
 
       let page = parseInt(req.query.page) || 1;
       let options = { sort: { createdAt: -1 }, limit: 10 };
 
       const messages = await Message.paginate(
-        { members: id },
+        { room: room.id },
         {
           options,
           page,
           populate: [
             { path: 'sender', select: ['username', 'createdAt'] },
-            { path: 'room', select: ['title', 'createdAt'] },
-            { path: 'lastMessage', select: ['content', 'createdAt'] },
+            { path: 'room', select: ['title', 'createdAt'], populate: [{ path: 'lastMessage', select: ['content', 'createdAt'] }] },
           ],
         }
       );
--- src/routers/api/api.js
+++ src/routers/api/api.js
@@ -3,10 +3,15 @@ const router = require('express').Router();
 //****controllers****
 const userController = require('../../controllers/api/user.controller');
 const roomController = require('../../controllers/api/room.controller');
+const messageController = require('../../controllers/api/message.controller');
 
 //****api****
+//****users****
 router.get('/users', userController.findMany);
+//****rooms****
 router.get('/rooms/:id', roomController.findManyRooms);
 router.delete('/rooms/:room/:id', roomController.deleteRooms);
+//****message****
+router.get('/msg/:id', messageController.findMany);
 
 module.exports = { apiRouter: router };
