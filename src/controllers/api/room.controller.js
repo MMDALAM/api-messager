ab7ee9c6e50aee41263516586586e7221bff7a5b
mohammad alamzadeh <mmmohammadalam@gmail.com>
2024-11-26 14:08:15 +0330

    fix

--- src/controllers/api/room.controller.js
+++ src/controllers/api/room.controller.js
@@ -3,6 +3,7 @@ const Room = require('../../models/room.model');
 const { isValidMongoId } = require('../../utils/function');
 const User = require('../../models/user.model');
 const Message = require('../../models/message.model');
+const { httpResSuccess, httpResError } = require('../../utils/httpResHelpers');
 
 class roomController extends controller {
   async findManyRooms(req, res, next) {
@@ -27,23 +28,23 @@ class roomController extends controller {
   async deleteRooms(req, res, next) {
     try {
       const { id, room } = req.params;
-      if (!isValidMongoId(id)) return res.status(200).json({ message: 'Not Valid MongoDB' });
-      if (!isValidMongoId(room)) return res.status(200).json({ message: 'Not Valid MongoDB' });
+      if (!isValidMongoId(id) || !isValidMongoId(room)) return res.status(400).json({ message: 'Invalid ID format provided' });
+
       const rooms = await Room.findOne({ _id: room });
-      if (!rooms) return res.status(404).json({ message: 'Room not found' });
+      if (!rooms) return httpResError(res, 404, 'Room Deleted');
+
       const user = await User.findById(id);
       if (!user) return res.status(404).json({ message: 'User not found' });
+
       if (rooms.admin.includes(id)) {
-        const messages = await Message.find({ room: rooms._id });
+        // const messages = await Message.find({ room: rooms._id });
 
-        if (messages)
-          messages.forEach(async (id) => {
-            await Message.findOneAndDelete({ id });
-          });
+        // if (messages)
+        await Message.deleteMany({ room: rooms.id });
 
-        await Room.findOneAndDelete({ _id: room });
+        await Room.findByIdAndDelete(rooms.id);
 
-        return res.status(200).json({ message: 'Room Deleted' });
+        return httpResSuccess(res, 200, 'Room Deleted', rooms);
       }
       return res.status(404).json({ message: 'No Access to delete room' });
     } catch (err) {
